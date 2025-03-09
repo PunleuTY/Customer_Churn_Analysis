bf2f36b967a201a1d2b0e57007505f9ce122bbcd
Sitharath-s <sitharothwhy@gmail.com>
2025-03-09 12:35:52 +0700

    modified correlation analysis to comapre only with churn

--- features_analysis/correlation_analysis.py
+++ features_analysis/correlation_analysis.py
@@ -10,19 +10,20 @@ class CorrelationAnalysis(BaseAnalysis):
     def perform_analysis(self):
         df = self.load_data()
 
-        features = ['Age','Tenure','Usage Frequency','Support Calls','Payment Delay','Total Spend','Last Interaction']
-        correlation_matrix = df[features].corr()
-
-        print(correlation_matrix)
+        features = ['Age', 'Tenure', 'Usage Frequency', 'Support Calls', 'Payment Delay', 'Total Spend', 'Last Interaction', 'Churn']
         
-        self.correlation_matrix(correlation_matrix)
-    
-    def correlation_matrix(self, correlation_matrix):
+        correlation_churn = df[features].corr()[['Churn']]
+
+        print(correlation_churn)
+        self.plot_correlation_heatmap(correlation_churn)
+
+    def plot_correlation_heatmap(self, correlation_matrix):
         sns.heatmap(correlation_matrix, annot=True, cmap="coolwarm", fmt=".2f", linewidths=0.4)
-        plt.title("Correlation Matrix of Features")
+        plt.title("Correlation Matrix with Churn")
         plt.show()
 
 if __name__ == "__main__":
     file_path = "d:/year2/term2/python/Project/Customer_Churn_Analysis/data/data_500_rec.csv"
     churn_analysis = CorrelationAnalysis(file_path)
     churn_analysis.perform_analysis()
+

867d6f2271b85681e85693dd8a9a2597ba82dc91
PUNLEU <punleutyy@gmail.com>
2025-03-09 20:35:30 +0700

    Add comment to all methods

--- features_analysis/age_analysis.py
+++ features_analysis/age_analysis.py
@@ -4,25 +4,15 @@ import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
 class AgeAnalysis(BaseAnalysis):
-    def perform_analysis(self):
-        print("Running Age Distribution Visualization : ")
-        self.visual_age_distribution()
 
-        print("Running Churn Rate by Age :")
-        self.visual_age_churn()
-
-        # Categorizing age groups
-        self.categorize_age_groups()
-        print("\nChurn Rate by Age Group:\n", self.df.groupby("Age Group")["Churn"].mean() * 100)
-        self.visual_age_churn_by_group()
-    
-        plt.show()
-
+    # Since our dataset contains range of age between 18 - 65 so we need to categorize and initial label of them.
+    # Bellow - [18-->30] : Young, [30-->50]: Middle-aged, [50-->65] : Olders
     def categorize_age_groups(self):
         bins = [18, 30, 50, 65]  # Define the age group ranges
         labels = ["Young", "Middle-aged", "Older"]
         self.df["Age Group"] = pd.cut(self.df["Age"], bins=bins, labels=labels, right=True)
 
+    # This is the visualization function of churn rate which affected by age groups using matplotlib & seaborn to create barplot
     def visual_age_churn_by_group(self):
         plt.figure(figsize=(8, 5))
         sns.barplot(x='Age Group', y='Churn', data=self.df, estimator=lambda x: np.mean(x) * 100, palette="coolwarm")
@@ -30,10 +20,11 @@ class AgeAnalysis(BaseAnalysis):
         plt.xlabel("Age Group")
         plt.ylabel("Churn Rate (%)")
 
-        
+    # Function to returns the distribution of age         
     def age_distribution(self):
         return self.df["Age"].value_counts()
     
+    # Function to visual the distribution of age frequency uses by using plt and syn libary to create hisplot
     def visual_age_distribution(self):
         plt.figure(figsize=(8, 5))
         sns.histplot(self.df['Age'], bins=20, kde=True, color="blue")
@@ -41,11 +32,13 @@ class AgeAnalysis(BaseAnalysis):
         plt.xlabel("Age")
         plt.ylabel("Frequency")
 
+    # Function to print the churn rate(%) by age group
     def age_churn(self):
         result = self.df.groupby("Age")["Churn"].mean() * 100
         print(result)
         return result
     
+    # Function to visual the age and churn rateas a boxplot
     def visual_age_churn(self):
         plt.figure(figsize=(8, 5))
         sns.boxplot(x='Churn', y='Age', data=self.df, palette="coolwarm")
@@ -53,8 +46,27 @@ class AgeAnalysis(BaseAnalysis):
         plt.xlabel("Churn (0 = No, 1 = Yes)")
         plt.ylabel("Age")
     
+    # Function to display all the analysis results
+    def perform_analysis(self):
 
+        # Display Age Distribution as a histplot
+        print("Running Age Distribution Visualization : ")
+        self.visual_age_distribution()
 
+        # Display Churn Rate which affected by age without categorize and print as a boxplot
+        print("Running Churn Rate by Age :")
+        self.visual_age_churn()
+
+        # Calling the categorize age function 
+        self.categorize_age_groups()
+        
+        # Display churn rate by age_groups as a barplot
+        print("\nChurn Rate by Age Group:\n", self.df.groupby("Age Group")["Churn"].mean() * 100)
+        self.visual_age_churn_by_group()
+    
+        plt.show()
+
+# Driver's code
 if __name__ == "__main__":
     data_path = "../data/data_500_rec.csv"
     age_analysis = AgeAnalysis(data_path)
--- features_analysis/subscription_analysis.py
+++ features_analysis/subscription_analysis.py
@@ -5,28 +5,24 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 class SubscriptionAnalysis(BaseAnalysis):
     
-    def perform_analysis(self):
-        print(f"\n Subscription Type Distribution : \n{self.subscription_distribution()}")
-        self.visual_subscription_distribution()
-        print(f"\n Churn Rate by Subscription Type :\n {self.subscription_churn()}")
-        self.visual_subscription_churn()
-
-        plt.show()
-
+    # Function to return the subscription distribution 
     def subscription_distribution(self):
         return self.df["Subscription Type"].value_counts()
     
+    # Function to display the subscription type distribution as a countplot
     def visual_subscription_distribution(self):
         plt.figure(figsize = (6,4))
         sns.countplot(x = "Subscription Type", data = self.df, palette = "coolwarm", order = self.df['Subscription Type'].value_counts().index )
         plt.title("Subscription Type Distribution of Customers")
         plt.xlabel("Subscription Type")
         plt.ylabel("Count")
-        plt.show()
+        
 
+    # Function to return the percentage of subscription type
     def subscription_churn(self):
         return self.df.groupby("Subscription Type")["Churn"].mean() * 100
     
+    # Function to display churn rate which affected by subscription type as a barplot
     def visual_subscription_churn(self):
         plt.figure(figsize=(6,4))
         sns.barplot(x=self.subscription_churn().index, y = self.subscription_churn().values,palette="coolwarm")
@@ -34,6 +30,19 @@ class SubscriptionAnalysis(BaseAnalysis):
         plt.xlabel("Subscription Type")
         plt.ylabel("Churn Rate (%)")
         plt.ylim(0, 100)
+
+    # Display all the analysis performance 
+    def perform_analysis(self):
+        # Display the subscription type distrinbution
+        print(f"\n Subscription Type Distribution : \n{self.subscription_distribution()}")
+        # Display a countplot to show the subscription type distribution
+        self.visual_subscription_distribution()
+        # Display Churn rate which affected by subscription type
+        print(f"\n Churn Rate by Subscription Type :\n {self.subscription_churn()}")
+        self.visual_subscription_churn()
+
+        plt.show()
+
 
 if __name__ == "__main__":
     data_path = "../data/data_500_rec.csv"
